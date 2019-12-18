0b2ad25615464cab077a60954c0f615fccb0251a
ShubhamMore <shubhammore1796@gmail.com>
2019-12-18 11:27:03 +0530

    way2success

--- src/email/mail.js
+++ src/email/mail.js
@@ -1,37 +1,54 @@
 const nodemailer = require('nodemailer');
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+    process.env.GMAIL_CLIENT_ID, // ClientID
+    process.env.GMAIL_CLIENT_SECRET, // Client Secret
+    "https://developers.google.com/oauthplayground" // Redirect URL
+);
+
+oauth2Client.setCredentials({
+    refresh_token: process.env.GMAIL_REFRESH_TOKEN
+});
+
+const accessToken = oauth2Client.getAccessToken()
 
 const sendMail = async (mail) => {
 
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-      service : "gmail",
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true, // true for 465, false for other ports
-      requireTLS: true,
-      auth: {
-          user: process.env.GMAIL_USER, // generated ethereal user
-          pass: process.env.GMAIL_PASSWORD // generated ethereal password
-      }
-  });
+    const smtpTransport = nodemailer.createTransport({
+        service: "gmail",
+        host: 'smtp.gmail.com',
+        auth: {
+            type: "OAuth2",
+            user: "shubhammore.developer@gmail.com", 
+            clientId: process.env.GMAIL_CLIENT_ID,
+            clientSecret: process.env.GMAIL_CLIENT_SECRET,
+            refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+            accessToken: accessToken
+        }
+    });
 
   const mailOptions = {
       from: mail.from, // sender address
       to: mail.to, // list of receivers
       subject: mail.subject, // Subject line
-      text: mail.text, // plain text body
+      generateTextFromHTML: true,
       html: mail.html // html body
   }
 
   // send mail with defined transport object
-  let info = await transporter.sendMail(mailOptions);
+    let info = await smtpTransport.sendMail(mailOptions);
 
-  console.log('Message sent: %s', info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    smtpTransport.close();
 
-  // Preview only available when sending through an Ethereal account
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 module.exports = sendMail;
